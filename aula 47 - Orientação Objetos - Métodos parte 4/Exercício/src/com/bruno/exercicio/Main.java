51f0cc508c14a776ae061fa6ecb90d6bea9dddd2
brunosc10699 <brunosc10699@gmail.com>
2022-09-17 21:55:02 -0300

    aula 47

---
+++ aula 47 - Orientação Objetos - Métodos parte 4/Exercício/src/com/bruno/exercicio/Main.java
@@ -0,0 +1,27 @@
+package com.bruno.exercicio;
+
+import com.bruno.exercicio.domain.Calculator;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Calculator calculator = new Calculator();
+
+        System.out.println(calculator.sumTwoNumbers(8.7, 7.9));
+
+        System.out.println(calculator.subtractingBetweenTwoNumbers(9.3, 6.2));
+
+        System.out.println(calculator.multiplyingTwoNumbers(9d, 7.1));
+
+        System.out.println(calculator.dividingTwoNumbers(10d, 0d));
+
+        System.out.println(calculator.dividingTwoNumbers(0d, 10d));
+
+        System.out.println(calculator.dividingTwoNumbers(5d, 7d));
+
+        calculator.printTheResultOfDividingTwoNumbers(20d, 35.7);
+
+        calculator.printTheResultOfDividingTwoNumbers(25.3, 0);
+    }
+}
---
+++ aula 47 - Orientação Objetos - Métodos parte 4/README.md
@@ -0,0 +1,12 @@
+# Aula 47
+
+## Orientação a Objetos - Métodos parte 4
+
+https://www.youtube.com/watch?v=d0BO5mBBFK8&list=PL62G310vn6nFIsOCC0H-C2infYgwm8SWW&index=48
+
+### Retorno parte 2
+
+> "Esses métodos que não possuem a palavra-chave void incorporada na  declaração, mas sim um tipo de dados, apresentam em seu corpo a palavra  reservada return, que informa que o método terá que retornar o mesmo  tipo de dados com o qual foi declarado."
+>
+> [Referência](https://www.devmedia.com.br/trabalhando-com-metodos-em-java/25917)
+
