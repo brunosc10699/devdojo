ea07e399c915409e1c4c50f909a3cefc82a0f1c4
brunosc10699 <brunosc10699@gmail.com>
2022-09-19 22:05:05 -0300

    aula 49

---
+++ aula 49 - Orientação Objetos - Métodos parte 6/Exercício/src/com/bruno/exercicio/domain/Student.java
@@ -0,0 +1,81 @@
+package com.bruno.exercicio.domain;
+
+import java.util.Objects;
+import java.util.Random;
+
+public class Student {
+
+    private Long id;
+    private String name;
+    private Integer age;
+    private Character gender;
+
+    public Student() {
+    }
+
+    public Student(Long id, String name, Integer age, Character gender) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Character getGender() {
+        return gender;
+    }
+
+    public void setGender(Character gender) {
+        this.gender = gender;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return id.equals(student.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "\nEstudante " + id +
+                "\nnome: " + name +
+                "\nidade: " + age +
+                "\nsexo: " + gender;
+    }
+
+    public void changeParameter(Student student) {
+        student.setAge(new Random().nextInt(101));
+        System.out.println("\nEstudante " + student.getId() + " DENTRO do método que muda o valor dos parâmetros:");
+        System.out.println(student);
+    }
+}
---
+++ aula 49 - Orientação Objetos - Métodos parte 6/README.md
@@ -0,0 +1,9 @@
+# Aula 49
+
+## Orientação a Objetos - Métodos parte 6
+
+https://www.youtube.com/watch?v=C-tmxo4FcKs&list=PL62G310vn6nFIsOCC0H-C2infYgwm8SWW&index=50
+
+### Parâmetros Tipo Referência
+
+Ao passar uma variável do tipo **referência** como parâmetro, para qualquer método, seu valor será alterado porque o que estará sendo enviado, ao método, é a referência do local em que se encontra o dado na memória. Dessa forma, o valor da variável é modificado.
